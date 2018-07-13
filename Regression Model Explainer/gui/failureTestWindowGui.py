466e1189701bd2a1a43726757bb58ea8c2fcba30
Martin Hoerletzberger <martin.hoerletzberger@gmail.com>
2018-07-14 00:11:27 +0200

    update failure test gui

--- Regression Model Explainer/gui/failureTestWindowGui.py
+++ Regression Model Explainer/gui/failureTestWindowGui.py
@@ -11,6 +11,7 @@ from tkinter.filedialog import askopenfilename
 
 from gui.mainWindowGui import MainWindowGui
 from services.explanationService import ExplanationService
+from gui.limeWindowGui import LimeWindowGui
 class FailureTestWindowGui(Frame):
     ''' Frame for the Failure Test
     '''
@@ -29,47 +30,25 @@ class FailureTestWindowGui(Frame):
         self.brbutton.pack(side='left')
         labelframe.pack(fill='x',padx=20)
         
-        self.emptyRow()
-        
         self.plotlabelframe=LabelFrame(self,text='Failure Test')
         self.plotframe=Frame(self.plotlabelframe)
         self.plotframe.pack(fill='both',expand='yes')
         self.plotlabelframe.pack(fill='both',expand='yes',padx=20)
         
-        self.emptyRow()
-        
-        labelframe=LabelFrame(self,text='Options')
-        self.ftbutton=Button(labelframe,text='Failure Test',command=self.failureTest)
-        self.ftbutton.pack(side='right')
-        self.cancelbutton=Button(labelframe,text='Cancel',command=lambda: master.switch_frame(MainWindowGui))
-        self.cancelbutton.pack(side='right') 
-        labelframe.pack(fill='x',padx=20)
-    def emptyRow(self):
-        frame=Frame(self,padx=20)
-        label = Label(frame,width=20, text='', anchor='e')
-        frame.pack(side='top',fill='x')
-        label.pack(side='left')
-    def disableMenueAndButtons(self):
-        self.master.disabelMenu()
-        self.brbutton.config(state='disabled')
-        self.ftbutton.config(state='disabled')
-        self.cancelbutton.config(state='disabled')
-    def enableMenueAndButtons(self):
-        self.master.enableMenu()
-        self.brbutton.config(state='normal')
-        self.ftbutton.config(state='normal')
-        self.cancelbutton.config(state='normal')
     def browseButton(self):
         file=askopenfilename(filetypes=[("CSV Files",".csv")])
+        self.setFileEntry(file)
+    def setFileEntry(self,file):
         self.fileentry.config(state='normal')
         self.fileentry.delete(0, END)
         self.fileentry.insert(0, file)
         self.fileentry.config(state='readonly')
+        self.failureTest()
     def failureTest(self):
         file=self.fileentry.get()
         data=ExplanationService.failureTest(file)
         if(data is None):
-            messagebox.showerror('Failure test failed', 'Todo: Infomation about the reason')
+            messagebox.showerror('Failure test failed', 'Data must fit to the model and model must be loaded')
             return
         self.showPlot(data)
     def on_plot_hover(self,event):
@@ -79,9 +58,14 @@ class FailureTestWindowGui(Frame):
             ind = self.line.contains(event)[1]["ind"]
             self.feat[ind[0]].set_visible(True)  
         self.canvas.draw_idle()
+    def on_click(self,event):
+        if(self.line.contains(event)[0]):
+            ind=self.line.contains(event)[1]['ind']
+            ind=ind[0]
+            file=self.fileentry.get()
+            self.master.switch_frame(LimeWindowGui)
+            self.master._frame.setFilePath(file,ind)
     def showPlot(self,data):
-        #print(data[0])
-        #print(data[4].__len__())
         
         messured=data[0]
         prediction=data[1]
@@ -94,16 +78,15 @@ class FailureTestWindowGui(Frame):
         a = f.add_subplot(111)
         self.line=a.scatter(messured, prediction, edgecolors=(0, 0, 0))
         a.plot([messured.min(), messured.max()], [messured.min(), messured.max()], 'k--', lw=1)
-        a.set_xlabel('Measured')
-        a.set_ylabel('Predicted')
+        a.set_xlabel('Measured('+data[2]+')')
+        a.set_ylabel('Predicted('+data[2]+')')
         
         self.feat=[]
         for i in range (data[3].__len__()):
             self.feat.append(a.annotate(data[3][i], (messured[i],prediction[i]),
                                         bbox=dict(boxstyle="round", fc="w")))
             self.feat[i].set_visible(False)
-        #canvas.mpl_connect('motion_notify_event', self.on_plot_hover) 
-        
+        f.suptitle('Failure test for: '+ data[2]+'\nmousover item to see features\nClick on item show explanation of the tuple (lime)')
         self.canvas = FigureCanvasTkAgg(f, master=self.plotframe)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side='top', fill='both', expand='yes')
@@ -111,4 +94,5 @@ class FailureTestWindowGui(Frame):
         toolbar = NavigationToolbar2Tk(self.canvas, self.plotframe)
         toolbar.update()
         self.canvas.mpl_connect('motion_notify_event', self.on_plot_hover)
+        self.canvas.mpl_connect('button_press_event',self.on_click)
         self.canvas._tkcanvas.pack(side='top', fill='both', expand='yes')
